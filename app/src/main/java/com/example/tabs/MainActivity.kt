9dbde2e733764aea8b4cf86c8c083e306bc6d743
ayush547 <ayushsinghs@yahoo.in>
2021-03-27 22:50:51 +0530

    Changed login UI

--- app/src/main/java/com/example/tabs/MainActivity.kt
+++ app/src/main/java/com/example/tabs/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.tabs
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
 import com.example.tabs.data.UserPreferences
@@ -15,14 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val userPreferences = UserPreferences(this)
-
-
-        userPreferences.authToken.asLiveData().observe(this, Observer {
-
-            val activity = if(it==null) AuthActivity::class.java else HomeActivity::class.java
-            startNewActivity(activity);
-        })
 
     }
 }
--- app/src/main/java/com/example/tabs/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/tabs/ui/auth/AuthActivity.kt
@@ -2,11 +2,22 @@ package com.example.tabs.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
 import com.example.tabs.R
+import com.example.tabs.data.UserPreferences
+import com.example.tabs.ui.home.HomeActivity
+import com.example.tabs.ui.startNewActivity
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val userPreferences = UserPreferences(this)
+        userPreferences.authToken.asLiveData().observe(this, Observer {
+            val activity = HomeActivity::class.java
+            if(it!=null) startNewActivity(activity);
+        })
     }
 }
