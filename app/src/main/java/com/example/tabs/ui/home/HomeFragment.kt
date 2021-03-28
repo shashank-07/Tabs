d362bd0d51e8ac22cabf0216b01237fdc430e98f
ayush547 <ayushsinghs@yahoo.in>
2021-03-28 14:56:17 +0530

    UI changes Modified the item cards for itinerary

--- app/src/main/java/com/example/tabs/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tabs/ui/home/HomeFragment.kt
@@ -42,22 +42,21 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 
-class HomeFragment  : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepository>(){
+class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepository>() {
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     val PERMISSION_ID = 1010
 
-     var userLocation:com.example.tabs.data.responses.Location=com.example.tabs.data.responses.Location(0.0,0.0)
-     var user_cluster:Int = -1
+    var userLocation: com.example.tabs.data.responses.Location =
+        com.example.tabs.data.responses.Location(0.0, 0.0)
+    var user_cluster: Int = -1
 
-    var budget:Int = -1
-    var time:Int = -1
-    var refresh:Int = 0
-    var api_being_called:Boolean=false
+    var budget: Int = -1
+    var time: Int = -1
+    var refresh: Int = 0
+    var api_being_called: Boolean = false
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-
-        closeCustom()
         intialize()
         checkLocal()
 
@@ -68,12 +67,11 @@ class HomeFragment  : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepos
                 is Resource.Success -> {
                     lifecycleScope.launch {
                         viewModel.saveCluster(it.value.response.cluster)
-                        Log.d("Callll","1")
+                        Log.d("Callll", "1")
                         binding.btBuild.performClick()
                     }
                 }
-                is Resource.Failure ->{
-
+                is Resource.Failure -> {
                     handleApiError(it) { getCluster(userLocation) }
                 }
             }
@@ -84,19 +82,17 @@ class HomeFragment  : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepos
         viewModel.itenaryResponse.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
             binding.progressbar.visible(it is Resource.Loading)
             when (it) {
-
                 is Resource.Success -> {
                     lifecycleScope.launch {
                         viewModel.setItenary(it.value.itenary)
-                        api_being_called=false
-                        Log.d("Hi",it.value.toString())
+                        api_being_called = false
+                        Log.d("Hi", it.value.toString())
                     }
                 }
-                is Resource.Failure ->{
+                is Resource.Failure -> {
 
                     handleApiError(it) {
-                        api_being_called=false
-
+                        api_being_called = false
                         buildItenary()
                     }
                 }
@@ -104,137 +100,125 @@ class HomeFragment  : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepos
         })
 
 
-
-        viewModel.itenary.observe(viewLifecycleOwner,{
-          val adapter=ItenaryRecyclerAdapter(viewModel, it, requireContext())
-              adapter.setHasStableIds(true);
-            binding.placesList.adapter=adapter
+        viewModel.itenary.observe(viewLifecycleOwner, {
+            val adapter = ItenaryRecyclerAdapter(viewModel, it, requireContext())
+            adapter.setHasStableIds(true);
+            binding.placesList.adapter = adapter
 
         })
 
         viewModel.userLoc.observe(viewLifecycleOwner, {
-            userLocation=it;
-           getCluster(userLocation)
+            userLocation = it;
+            getCluster(userLocation)
 
         })
-        viewModel.userCluster.observe(viewLifecycleOwner,{
-            user_cluster=it
+        viewModel.userCluster.observe(viewLifecycleOwner, {
+            user_cluster = it
         })
-        viewModel.budget.observe(viewLifecycleOwner,{
-            budget=it
+        viewModel.budget.observe(viewLifecycleOwner, {
+            budget = it
         })
-        viewModel.time.observe(viewLifecycleOwner,{
-            time=it
+        viewModel.time.observe(viewLifecycleOwner, {
+            time = it
         })
-        viewModel.refresh.observe(viewLifecycleOwner,{
-            refresh=it
+        viewModel.refresh.observe(viewLifecycleOwner, {
+            refresh = it
             binding.btBuild.performClick()
         })
 
 
 
         binding.refresh.setOnClickListener {
-            viewModel.setRefresh(refresh+1);
+            viewModel.setRefresh(refresh + 1);
         }
-        binding.btBuild.setOnClickListener{
-            if(user_cluster==-1){
+        binding.btBuild.setOnClickListener {
+            if (user_cluster == -1) {
                 RequestPermission()
                 getLastLocation()
-
-            }else{
+            } else {
                 buildItenary()
                 closeCustom()
             }
-
         }
-
 
 
         binding.customize.setOnClickListener {
+            if (binding.hiddenView.visibility == View.VISIBLE) {
+                TransitionManager.beginDelayedTransition(
+                    binding.mainCard,
+                    AutoTransition()
+                )
+                binding.hiddenView.visibility = View.GONE
 
-
-
-                if (binding.hiddenView.visibility == View.VISIBLE) {
-
-
-                    TransitionManager.beginDelayedTransition(binding.mainCard,
-                        AutoTransition())
-                    binding.hiddenView.visibility = View.GONE
-
-                } else {
-                    TransitionManager.beginDelayedTransition(binding.mainCard,
-                        AutoTransition())
-                    binding.hiddenView.visibility = View.VISIBLE
-                }
-
-
-
+            } else {
+                TransitionManager.beginDelayedTransition(
+                    binding.mainCard,
+                    AutoTransition()
+                )
+                binding.hiddenView.visibility = View.VISIBLE
+            }
         }
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+    }
 
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
-
+    fun closeCustom() {
+        binding.hiddenView.visibility = View.GONE
 
     }
 
-    fun closeCustom(){
-        binding.hiddenView.visibility=View.GONE
-
-    }
-    private fun intialize(){
+    private fun intialize() {
         binding.progressbar.visible(false)
-        binding.placesList.layoutManager= LinearLayoutManager(requireContext())
+        binding.placesList.layoutManager = LinearLayoutManager(requireContext())
 
 
     }
 
 
-    private fun getCluster(userLocation:com.example.tabs.data.responses.Location) {
-        if(!userLocation.equals(null)){
-            if(userLocation.latitude!=0.0){
+    private fun getCluster(userLocation: com.example.tabs.data.responses.Location) {
+        if (!userLocation.equals(null)) {
+            if (userLocation.latitude != 0.0) {
                 viewModel.getCluster(userLocation.latitude, userLocation.longitude)
-
-            }else{
+            } else {
                 view?.snackbar("Your location has not been found \nUse default location to by pass this")
             }
         }
+    }
+
+
+    fun checkLocal() {
+        userPreferences.cluster.asLiveData()
+            .observe(requireActivity(), androidx.lifecycle.Observer {
+                Log.d("local", "got cluster" + it.toString())
+                if (it != null) {
+                    viewModel.setCluster(it)
+                }
+
+            })
 
 
     }
 
+    private fun buildItenary() {
 
-    fun checkLocal(){
-        userPreferences.cluster.asLiveData().observe(requireActivity(), androidx.lifecycle.Observer {
-            Log.d("local","got cluster"+it.toString())
-            if(it!=null){
-                viewModel.setCluster(it)
-            }
-
-        })
-
-
-    }
-
-    private fun buildItenary(){
-
-        if(binding.etBudget.text.toString()!=""){
-            budget=binding.etBudget.text.toString().trim().toInt()
+        if (binding.etBudget.text.toString() != "") {
+            budget = binding.etBudget.text.toString().trim().toInt()
 
         }
-        if(binding.etTime.text.toString()!=""){
-            time=binding.etTime.text.toString().trim().toInt()
+        if (binding.etTime.text.toString() != "") {
+            time = binding.etTime.text.toString().trim().toInt()
 
         }
 
-        if(budget!=-1&&time!=-1&&user_cluster!=-1){
-            if(!api_being_called){
-                api_being_called=true
+        if (budget != -1 && time != -1 && user_cluster != -1) {
+            if (!api_being_called) {
+                api_being_called = true
 
-                viewModel.getItenary(budget,time,refresh,user_cluster)
+                viewModel.getItenary(budget, time, refresh, user_cluster)
 
             }
 
-        }else{
+        } else {
             view?.snackbar("Did not get budget/time/location")
 
         }
@@ -242,58 +226,75 @@ class HomeFragment  : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepos
     }
 
     @SuppressLint("MissingPermission")
-    fun getLastLocation(){
-        if(CheckPermission()){
-            if(isLocationEnabled()){
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener {task->
-                    var location:Location? = task.result
-                    if(location == null){
+    fun getLastLocation() {
+        if (CheckPermission()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
                         NewLocationData()
-                    }else{
-                        viewModel.setLoc(location.latitude,location.longitude)
-                        Log.d("Debug:" , "You Current Location is : Long: "+ location.longitude + " , Lat: " + location.latitude)
+                    } else {
+                        viewModel.setLoc(location.latitude, location.longitude)
+                        Log.d(
+                            "Debug:",
+                            "You Current Location is : Long: " + location.longitude + " , Lat: " + location.latitude
+                        )
                     }
                 }
-            }else{
-                Toast.makeText(requireContext(),"Please Turn on Your device Location",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Please Turn on Your device Location",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-        }else{
+        } else {
             RequestPermission()
         }
     }
 
 
     @SuppressLint("MissingPermission")
-    fun NewLocationData(){
-        var locationRequest =  LocationRequest()
+    fun NewLocationData() {
+        var locationRequest = LocationRequest()
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         locationRequest.interval = 0
         locationRequest.fastestInterval = 0
         locationRequest.numUpdates = 1
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
         fusedLocationProviderClient!!.requestLocationUpdates(
-            locationRequest,locationCallback,Looper.myLooper()
+            locationRequest, locationCallback, Looper.myLooper()
         )
     }
 
 
-    private val locationCallback = object : LocationCallback(){
+    private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             var lastLocation: Location = locationResult.lastLocation
-            viewModel.setLoc(lastLocation.latitude,lastLocation.longitude)
-            Log.d("Debug:" , "You Current Location is Result : Long: "+ lastLocation.longitude + " , Lat: " + lastLocation.latitude)
+            viewModel.setLoc(lastLocation.latitude, lastLocation.longitude)
+            Log.d(
+                "Debug:",
+                "You Current Location is Result : Long: " + lastLocation.longitude + " , Lat: " + lastLocation.latitude
+            )
 
         }
     }
 
-    private fun CheckPermission():Boolean{
+    private fun CheckPermission(): Boolean {
         //this function will return a boolean
         //true: if we have permission
         //false if not
-        if(
-            ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(requireContext(),android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-        ){
+        if (
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             return true
         }
 
@@ -301,20 +302,26 @@ class HomeFragment  : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepos
 
     }
 
-    fun RequestPermission(){
+    fun RequestPermission() {
         //this function will allows us to tell the user to requesut the necessary permsiion if they are not garented
         ActivityCompat.requestPermissions(
             requireActivity(),
-            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
             PERMISSION_ID
         )
     }
 
-    fun isLocationEnabled():Boolean{
+    fun isLocationEnabled(): Boolean {
         //this function will return to us the state of the location service
         //if the gps or the network provider is enabled then it will return true otherwise it will return false
-        var locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        var locationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
     }
 
 
@@ -323,24 +330,23 @@ class HomeFragment  : BaseFragment<HomeViewModel, FragmentHomeBinding, MainRepos
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        if(requestCode == PERMISSION_ID){
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Log.d("Debug:","You have the Permission")
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug:", "You have the Permission")
             }
         }
     }
 
 
-
-    override fun getViewModel() = HomeViewModel ::class.java
+    override fun getViewModel() = HomeViewModel::class.java
 
     override fun getFragmentBinding(
-            inflater: LayoutInflater,
-            container: ViewGroup?
-    )= FragmentHomeBinding.inflate(inflater, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentHomeBinding.inflate(inflater, container, false)
 
     override fun getFragmentRepository() =
-            MainRepository(remoteDataSource.buildApi(Api::class.java), userPreferences)
+        MainRepository(remoteDataSource.buildApi(Api::class.java), userPreferences)
 
 
 }
